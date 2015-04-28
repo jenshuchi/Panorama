5ba02131ab9506a21475bea65c407d172fb87520
jack jen <jack115347@gmail.com>
2015-04-28 09:50:03 +0800

    working on ConvertMat, but it still has bugs...

--- HarrisCorner.cpp
+++ HarrisCorner.cpp
@@ -4,13 +4,17 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <vector>
+#include <typeinfo>
 
 using namespace std;
 using namespace cv;
 
 #define PI 3.1415926
+#define WIN_SIZE 5
 
-int **HarrisCorner( Mat image );
+void ConvertMat( Mat src, Mat &dst );
+void PrintMat( Mat &m );
+void GrayScale( Mat image, Mat &gray );
 void Convolution( Mat image, Mat &result, float ***filter, int window_size );
 void GaussianFilter( float ***filter,  int window_size, float theta );
 void Gradient( float ***filter_in, float ***filter_out, char var, int window_size );
@@ -21,24 +25,57 @@ int width, height;
 int main( int argc, char **argv)
 {
 	Mat image = imread( argv[1], 1 );
-	Mat gray, gaus;
+	Mat gray, gray2, gaus;
 	Mat result_x, result_y;
+	Mat draw;
 	float **filter_gaus, **filter_x, **filter_y;
+
+	GrayScale( image, gray );
 	
-	// opencv3 change: CV_RGB2GRAY => COLOR_RGB2GRAY
-	cvtColor( image, gray, COLOR_BGR2GRAY );
+	// ?????
+	//cout << image.size[0] << " " << image.size[1] << " " << image.size[2]  << " " << gray.size[0] << " " << gray.size[1] << endl;
 	
-	GaussianFilter( &filter_gaus, 5, 1 );
-	Gradient( &filter_gaus, &filter_x, 'x', 5 );
-	Gradient( &filter_gaus, &filter_y, 'y', 5 );
-	Convolution( gray, gaus, &filter_gaus, 5 );
-	Convolution( gaus, result_x, &filter_x, 5 );
-	Convolution( gaus, result_y, &filter_y, 5 );
+	/* opencv3 change: CV_RGB2GRAY => COLOR_RGB2GRAY */
+	//cvtColor( image, gray, COLOR_BGR2GRAY );
+	
+	/* print filter
+	for(int i=0;i<WIN_SIZE;i++){
+		for(int j=0;j<WIN_SIZE;j++)
+			cout << filter_gaus[i][j] << " ";
+		cout << endl;
+	}
+	*/
+	GaussianFilter( &filter_gaus, WIN_SIZE, 1 );
+	
+	Gradient( &filter_gaus, &filter_x, 'x', WIN_SIZE );
+	Gradient( &filter_gaus, &filter_y, 'y', WIN_SIZE );
+	
+	Convolution( gray, gaus, &filter_gaus, WIN_SIZE );
+	Convolution( gaus, result_x, &filter_x, WIN_SIZE );
+	Convolution( gaus, result_y, &filter_y, WIN_SIZE );
+
+	Mat Ixx = result_x.mul(result_x);
+	Mat Iyy = result_y.mul(result_y);
+	Mat Ixy = result_x.mul(result_y);
+
+	Mat detM = Ixx.mul(Iyy) - Ixy.mul(Ixy);
+	Mat trM = Ixx + Iyy;
+	float k = 0.05;
+	Mat R = detM - k * trM.mul(trM);
+	/* print matrix */
+	//PrintMat(gray);
+
+	//cout << image.type() << endl;
+	//cout << gray.type() << endl;
+	//cout << gaus.type() << endl;
+	//cout << result_x.type() << endl;
 
 	free(filter_gaus);
 	free(filter_x);
 	free(filter_y);
-/*
+	
+	/* opencv gradient test */
+	/*
 	Mat grad_x, grad_y;
 	Mat abs_grad_x, abs_grad_y;
 	
@@ -51,22 +88,78 @@ int main( int argc, char **argv)
 
 	Sobel( gaus, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
 	convertScaleAbs( grad_y, abs_grad_y );
-*/
-
+	*/
+	
+	//PrintMat(gray);	
+	/* cannot use opencv convertTo() successfully, so write another ConvertMat() */
+	/*
+	double minVal, maxVal;
+	minMaxLoc(gray, &minVal, &maxVal);
+	gray.convertTo(draw, CV_8U);
+	*/
+	ConvertMat( result_x, draw );
+	PrintMat(result_x);	
 
 	namedWindow( "Display Image", WINDOW_AUTOSIZE );
-	imshow( "Display Image", gaus );
+	imshow( "Display Image", draw);
 
 	waitKey(0);
 	image.release();
 	return 0;
 }
 
-int **HarrisCorner( Mat image )
+//void ImageF2C()
+//{
+//}
+
+void ConvertMat( Mat src, Mat &dst )
 {
+	dst.create( src.size(), CV_8U );
+	for( int i=0 ; i < src.rows ; i++ )
+	{
+		for( int j=0 ; j < src.cols ; j++ )
+		{
+			dst.data[i*dst.step[0] + j*dst.step[1]] = (uchar)src.data[i*src.step[0] + j*src.step[1]];
+		}
+	}
 }
 
-// passing pixel on the edge will cause problem
+void PrintMat( Mat &m )
+{
+	int row = m.rows;
+	int col = m.cols;
+
+	for( int i=0 ; i < row ; i++ )
+	{
+		for( int j=0 ; j < col ; j++ )
+		{
+			cout << (float)m.data[ i*m.step[0]+j*m.step[1] ] << " ";
+		}
+		cout << endl;
+	}
+}
+
+/* compute the intensity of image */
+void GrayScale( Mat image, Mat &gray )
+{
+	float sum;
+
+	gray.create( image.rows, image.cols, CV_32FC1 );
+
+	for( int i=0 ; i < image.rows ; i++ )
+	{
+		for( int j=0 ; j < image.cols ; j++ )
+		{	
+			sum  = 0.299 * (float)image.data[ i*image.step[0] + j*image.step[1] + 0*image.step[0] ];
+			sum += 0.587 * (float)image.data[ i*image.step[0] + j*image.step[1] + 0*image.step[1] ];
+			sum += 0.114 * (float)image.data[ i*image.step[0] + j*image.step[1] + 0*image.step[2] ];
+			gray.data[ i*gray.step[0] + j*gray.step[1] ] = sum;
+			//cout << sum << endl;
+		}
+	}
+}
+
+/* passing pixel on the edge will cause problem */
 void FindM( Mat &image, Mat &M, int x, int y, int window_size )
 {
 	//float Ix = Gradient( image, x, y, 'x', window_size );
@@ -76,13 +169,13 @@ void FindM( Mat &image, Mat &M, int x, int y, int window_size )
 	//M = ( Mat_<uchar>(2,2) << Ix*Ix, Ix*Iy, Ix*Iy, Iy*Iy );
 }
 
-// do gradient on a filter
+/* do gradient on a filter */
 void Gradient( float ***filter_in, float ***filter_out, char var, int window_size )
 {
 	int half_ksize = window_size / 2;
 	float tmp, sum = 0;
 	*filter_out = (float**)malloc( sizeof(float*) * window_size );
-	
+
 	for( int i = -half_ksize ; i <= half_ksize ; i++ )
 	{
 		(*filter_out)[i+half_ksize] = (float*)malloc( sizeof(float) * window_size );
@@ -96,7 +189,7 @@ void Gradient( float ***filter_in, float ***filter_out, char var, int window_siz
 	}
 }
 
-// set up Gaussian filter
+/* set up Gaussian filter */
 void GaussianFilter( float ***filter,  int window_size, float theta )
 {
 	int half_ksize = window_size / 2;
@@ -117,27 +210,39 @@ void GaussianFilter( float ***filter,  int window_size, float theta )
 			(*filter)[i][j] = (*filter)[i][j] / sum;
 }
 
+/* do convolution */
 void Convolution( Mat image, Mat &result, float ***filter, int window_size )
 {
-	int width = image.cols;
-	int height = image.rows;
+	int col = image.cols;
+	int row = image.rows;
 	int half_ksize = window_size / 2;
+
 	result.create( image.size(), image.type() );
-	// do convolution
-	for( int i = half_ksize ; i < height - half_ksize ; i++ )
+
+	for( int i = 0 ; i < row ; i++ )
 	{
-		for( int j = half_ksize ; j < width - half_ksize ; j++ )
+		for( int j = 0 ; j < col ; j++ )
 		{
+			int up = -half_ksize, down = half_ksize;
+			int left = -half_ksize, right = half_ksize;
 			float sum = 0;
-			for( int y = -half_ksize ; y <= half_ksize ; y++ )
+			float de = 0;
+
+			if( i < half_ksize ) up = -i;
+			if( i > row - half_ksize ) down = row - i - 1;
+			if( j < half_ksize ) left = -j;
+			if( j > col -half_ksize ) right = col -j -1;
+
+			for( int y = up ; y <= down ; y++ )
 			{
-				for( int x = -half_ksize ; x <= half_ksize ; x++ )
+				for( int x = left ; x <= right ; x++ )
 				{
-					sum += (float)image.data[ (i+y)*width + j+x ] * (*filter)[y+half_ksize][x+half_ksize];
+					de += (*filter)[y+half_ksize][x+half_ksize];
+					sum += (float)image.data[ (i+y)*image.step[0] + (j+x)*image.step[1] ] * (*filter)[y+half_ksize][x+half_ksize];
 				}
 			}
-//	cout << "gaus filter: ( " << i << ", " << j << " )" << endl;
-			result.data[ i*width + j ] = sum;
+			result.data[ i*result.step[0] + j*result.step[1] ] = sum;// / de;
+			//cout << sum / de << endl;
 		}
 	}
 }
